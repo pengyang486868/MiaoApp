19cf96c57c7b132531f286aab1ea12aabeb3d711
yang peng <854525261@qq.com>
2018-11-13 11:14:33 +0800

    hint try

--- DialogAppBase/DialogAppWindowBase/DialogManager.cs
+++ DialogAppBase/DialogAppWindowBase/DialogManager.cs
@@ -8,24 +8,43 @@ namespace DialogAppWindowBase
     {
         public static void ShowDialog(ViewModelBase vm, string title = null, int height = 300, int width = 500)
         {
+            var vIns = CreateViewIns(vm);
+            if (vIns == null) return;
+
+            var wnd = new PlainDialog
+            {
+                Title = title,
+                Height = height,
+                Width = width,
+                DataContext = vm,
+                MainControl = { Content = vIns as UserControl }
+            };
+            wnd.ShowDialog();
+        }
+
+
+        public static void ShowHintWindow(string str)
+        {
+            var hwin = new HintWindow()
+            {
+                TxtBlock = { Text = str },
+                Opacity = 1
+            };
+            hwin.Show();
+        }
+
+        public static object CreateViewIns(object vm)
+        {
             var vmType = vm.GetType();
             var vmTypeName = vmType.FullName;
-            if (vmTypeName != null)
-            {
-                var vName = vmTypeName.Replace("ViewModel", "View");
-                var vInsType = vmType.Assembly.GetType(vName);
-                var vIns = Activator.CreateInstance(vInsType);
+            if (vmTypeName == null) return null;
 
-                var wnd = new PlainDialog
-                {
-                    Title = title,
-                    Height = height,
-                    Width = width,
-                    DataContext = vm,
-                    MainControl = {Content = vIns as UserControl}
-                };
-                wnd.ShowDialog();
-            }
+            var vName = vmTypeName.Replace("ViewModel", "View");
+            var vInsType = vmType.Assembly.GetType(vName);
+            var vIns = Activator.CreateInstance(vInsType);
+            return vIns;
         }
+
+
     }
 }
---
+++ DialogAppBase/DialogAppWindowBase/HintWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace DialogAppWindowBase
+{
+    /// <summary>
+    /// HintWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class HintWindow
+    {
+        public HintWindow()
+        {
+            InitializeComponent();
+        }
+
+        public new void Show()
+        {
+            base.Show();
+            var anim = new DoubleAnimation
+            {
+                From = Opacity,
+                To = 0,
+                Duration = new Duration(TimeSpan.FromSeconds(3))
+            };
+            anim.Completed += Anim_Completed;
+            BeginAnimation(OpacityProperty, anim);
+
+            var animt = new DoubleAnimation
+            {
+                From = TxtBlock.Opacity,
+                To = 0.2,
+                Duration = new Duration(TimeSpan.FromSeconds(3))
+            };
+            BeginAnimation(OpacityProperty, animt);
+        }
+
+        private void Anim_Completed(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
